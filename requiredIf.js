d0f5793ef115c7bdc198d131d433bedf459cf1be
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-06-02 20:15:56 -0300

    Copy code from another project

---
+++ requiredIf.js
@@ -0,0 +1,11 @@
+import { isNullOrUndefined, isEmptyArray } from "./utils";
+
+const message = "El campo es obligatorio.";
+
+function validate(value) {
+  if (isNullOrUndefined(value) || isEmptyArray(value)) return false;
+  if (value === false) return false;
+  return !!String(value).trim().length;
+}
+
+export default required_if => value => (required_if ? validate(value) || message : true);
