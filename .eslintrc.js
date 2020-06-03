912299d607eb9c19d0a9e7676a299317c4b74807
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-06-02 22:45:07 -0300

    Add up config base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
+  parserOptions: {
+    ecmaVersion: 8 // or 2017
   },
-  extends: ["eslint:recommended"],
   globals: {
   },
-  plugins: [],
-  rules: {},
+  rules: {  },
+  extends: ["prettier", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  env: {
+    node: true, // Node.js global variables and Node.js scoping.
+    es6: true, // enable all ECMAScript 6 features except for modules.
+  }
 };
