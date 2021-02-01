df9e165f6e2fa00b45580a4c0dce8028d8c19a1d
Marani Matias Ezequiel <maranimatias@gmail.com>
2021-02-01 15:10:07 -0300

    Update cash.js

--- src/cash.js
+++ src/cash.js
@@ -1,6 +1,6 @@
 const cashValid = /^([1-9]{1}\d*|0)([,.]\d{1,2})?$/;
 // const message = "El campo solo puede un moto valido.";
-const message = "Monto no valido.";
+const message = "Monto no válido.";
 
 /**
  * @name Validation
